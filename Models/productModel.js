7d63144f895e49d1f6ae1f0e93b09a98f427f89e
VaibhavSharma1998 <sharmavaibhav203@gmail.com>
2023-09-30 23:59:16 +0530

    "checking update"

--- Models/productModel.js
+++ Models/productModel.js
@@ -37,7 +37,7 @@ const productSchema = new mongoose.Schema({
     required:[true,'Please describe the role/gender']
   },
   love:{
-    type:String
+    type:Stri
   },
   createdAt: {
     type: Date,
